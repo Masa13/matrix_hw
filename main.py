33cf484435538858996d26a79c1c112b52b057a0
Masa13 <masahero55@gmail.com>
2016-03-07 03:24:06 -0500

    edits

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ from math import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-m = [[0,200,200,0],
+m0 = [[0,200,200,0],
      [100,0,0,100],
      [0,100,100,0],
      [1,1,1,1]]
-          
-add_edge(m, 100,0,100,0,100,0)
-add_edge(m, 0,200,0,200,0,200)
-add_edge(m, 100,100,0,0,0,100)
-add_edge(m, 100,100,00,0,0,0)
-add_edge(m, 0,0,200,200,0,0)
+draw_lines(m0, screen, color)
 
+m1 = matrix_mult((make_translate(100,100,0)),m0)
 
-draw_lines(m, screen, color)
-m = matrix_mult(make_translate(100,100,0),m)
-m = matrix_mult(make_rotX(pi),m)
-draw_lines(m, screen, color)
+m2 =  matrix_mult(make_rotX(pi),m0)
+
+draw_lines(m0,screen,color)
+draw_lines(m1,screen,color)
+draw_lines(m2,screen,color)
+
 
 display(screen)
