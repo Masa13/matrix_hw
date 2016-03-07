0f2ed081333f754bb0ea33df87ce8c2316234a93
Masa13 <masahero55@gmail.com>
2016-03-07 03:26:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ m0 = [[0,200,200,0],
      [100,0,0,100],
      [0,100,100,0],
      [1,1,1,1]]
-draw_lines(m0, screen, color)
 
-m1 = matrix_mult((make_translate(100,100,0)),m0)
-
+m1 = matrix_mult(make_translate(100,100,0),m0)
 m2 =  matrix_mult(make_rotX(pi),m0)
 
 draw_lines(m0,screen,color)
