c490ea6068abc404b25493d4b00223b77eb5d825
Masa13 <masahero55@gmail.com>
2016-03-07 03:15:26 -0500

    dfa

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 
 m = [[0,200,200,0],
-     [10,0,0,100],
-     [0,0,0,0],
+     [100,0,0,100],
+     [0,100,100,0],
      [1,1,1,1]]
           
-add_edge(m, 10,0,10,0,10,0)
-add_edge(m, 0,80,0,80,0,80)
-add_edge(m, 30,30,0,0,0,30)
-add_edge(m, 10,10,10,10,10,10)
-add_edge(m, 0,0,0,50,0,0)
+add_edge(m, 100,0,100,0,100,0)
+add_edge(m, 0,200,0,200,0,200)
+add_edge(m, 100,100,0,0,0,100)
+add_edge(m, 100,100,00,0,0,0)
+add_edge(m, 0,0,200,200,0,0)
 
 m = matrix_mult(make_translate(100,100,0),m)
 m = matrix_mult(make_rotX(pi),m)
