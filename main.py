ff0ca3499c33249175c29fe16d30c78e1040c2a8
Masa13 <masahero55@gmail.com>
2016-03-07 04:26:04 -0500

    owo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from draw import *
 from math import *
 
 screen = new_screen()
-color1 = [ 0, 255, 0 ]
+color1 = [ 0, 0, 255 ]
 color2 = [255,0,0]
 
 m0 = [[0,200,200,0],
@@ -21,16 +21,35 @@ n = 0
 m2 = m0
 m3 = m1
 
+m2 = matrix_mult(make_translate(50,50,0),m2)
+m3 = matrix_mult(make_translate(50,50,0),m2)
+
+while n < 360:
+    m2 = matrix_mult(make_rotX(pi/13),m2)
+    m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
+    draw_lines(m2,screen,color1)
+    draw_lines(m3,screen,color2)
+    n+=1
+    
 m2 = matrix_mult(make_translate(100,100,0),m2)
 m3 = matrix_mult(make_translate(100,100,0),m2)
 
-while n < 3600:
+while n < 360:
     m2 = matrix_mult(make_rotX(pi/13),m2)
     m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
     draw_lines(m2,screen,color1)
     draw_lines(m3,screen,color2)
     n+=1
 
+m2 = matrix_mult(make_translate(250,250,0),m2)
+m3 = matrix_mult(make_translate(250,250,0),m2)
+
+while n < 360:
+    m2 = matrix_mult(make_rotX(pi/13),m2)
+    m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
+    draw_lines(m2,screen,color1)
+    draw_lines(m3,screen,color2)
+    n+=1
 
     
 #draw_lines(m0,screen,color)
