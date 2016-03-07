29670b0d8bc7723f8dc1c41d67f88f05a53dcc98
Masa13 <masahero55@gmail.com>
2016-03-07 04:29:59 -0500

    qwq

--- main.py
+++ main.py
@@ -16,48 +16,26 @@ m1 = [[100,100,100,100],
       [0,0,300,300],
       [1,1,1,1]]
 #m1 = matrix_mult(make_translate(100,100,0),m0)
-
-n = 0
-m2 = m0
-m3 = m1
-
-m2 = matrix_mult(make_translate(50,50,0),m2)
-m3 = matrix_mult(make_translate(50,50,0),m2)
-
-while n < 360:
-    m2 = matrix_mult(make_rotX(pi/13),m2)
-    m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
-    draw_lines(m2,screen,color1)
-    draw_lines(m3,screen,color2)
-    n+=1
-
-n = 0
-m2 = m0
-m3 = m1
-    
-m2 = matrix_mult(make_translate(100,100,0),m2)
-m3 = matrix_mult(make_translate(100,100,0),m2)
-
-while n < 360:
-    m2 = matrix_mult(make_rotX(pi/13),m2)
-    m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
-    draw_lines(m2,screen,color1)
-    draw_lines(m3,screen,color2)
-    n+=1
-
-n = 0
-m2 = m0
-m3 = m1
-
-m2 = matrix_mult(make_translate(250,250,0),m2)
-m3 = matrix_mult(make_translate(250,250,0),m2)
-
-while n < 360:
-    m2 = matrix_mult(make_rotX(pi/13),m2)
-    m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
-    draw_lines(m2,screen,color1)
-    draw_lines(m3,screen,color2)
-    n+=1
+i = 0
+while i < 500:
+     n = 0
+     n = 0
+     m2 = m0
+     m3 = m1
+     x = 0
+     y = 0
+     while n < 360:
+          m2 = matrix_mult(make_translate(x,y,0),m2)
+          m3 = matrix_mult(make_translate(x,y,0),m2)
+          
+          m2 = matrix_mult(make_rotX(pi/13),m2)
+          m3 = matrix_mult(make_scale(1.1,1.1,1),m2)  
+          draw_lines(m2,screen,color1)
+          draw_lines(m3,screen,color2)
+          n+=1
+     x+=20
+     y+=20
+     i+=1
 
     
 #draw_lines(m0,screen,color)
