185464bd659bd7d442c8a1618a478f2f9007f80e
Masa13 <masahero55@gmail.com>
2016-03-07 02:49:43 -0500

    .-.

--- matrix.py
+++ matrix.py
@@ -1,19 +1,37 @@
 import math
 
 def make_translate( x, y, z ):
-    pass
+    return [[1,0,0,x],
+            [0,1,0,y],
+            [0,0,1,z],
+            [0,0,0,1]]
 
 def make_scale( x, y, z ):
-    pass
+    return [[x,0,0,0],
+            [0,y,0,0],
+            [0,0,z,0],
+            [0,0,0,1]]
     
 def make_rotX( theta ):   
-    pass
+    t = radians(theta)
+    return [[1,0,0,0],
+            [0,cos(t),-sin(t),0],
+            [0,sin(t),cos(t),0],
+            [0,0,0,1]]
 
 def make_rotY( theta ):
-    pass
+    t = radians(theta)
+    return [[cos(t),0,-sin(t),0],
+            [0,1,0,0],
+            [sin(t),0,cos(t),0],
+            [0,0,0,1]]
 
 def make_rotZ( theta ):
-    pass
+    t = radians(theta)
+    return [[cos(t),-sin(t),0,0],
+            [sin(t),cos(t),0,0],
+            [0,0,1,0],
+            [0,0,0,1]]
 
 def new_matrix(rows = 4, cols = 4):
     m = []
@@ -24,14 +42,32 @@ def new_matrix(rows = 4, cols = 4):
     return m
 
 def print_matrix( matrix ):
-    pass
+    m = ''
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            m += str(matrix[x][y])
+            m += '\t'
+        m += '\n'
+    print m
 
 def ident( matrix ):
-    pass
+    m = new_matrix(len(matrix),len(matrix))
+    for x in range(len(matrix)):
+        m[x][x] = 1
+    return m
 
-def scalar_mult( matrix, x ):
-    pass
+def scalar_mult( matrix, n ):
+    m = new_matrix(len(matrix[0]), len(matrix))
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            m[x][y] = n * matrix[x][y]
+    return m
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+    m = new_matrix(len(m2[0]),len(m1))
+    for x in range(len(m1)):
+        for y in range(len(m2[0])):
+            for i in range(len(m2)):
+                m[x][y] = m[x][y] + m1[x][i] * m2[i][y]
+    return m
